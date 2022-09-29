e39bd0b9e37b5fac64a4e8a9fca5e235ca122fb2
xiao kaiwen <xiaokw@dragonpass.com.cn>
2022-09-29 21:09:53 +0800

    chore: any -> unknown

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,10 +1,10 @@
 // 0在业务上是一个真值，而不是js的假值(一个！为求反，两个为求反后的boolean) !undefined === true -- !!0 === false
 import { useEffect } from "react";
 import { useState } from "react";
-export const isFalsy = (value) => (value === 0 ? false : !value);
+export const isFalsy = (value: unknown): boolean => (value === 0 ? false : !value);
 
 // 在一个函数中，改变传入的对象本身是不好的
-export const cleanObject = (object) => {
+export const cleanObject = (object: object) => {
   // Object.assign({}, object)
   const result = { ...object };
   Object.keys(result).forEach((key) => {
@@ -16,13 +16,14 @@ export const cleanObject = (object) => {
   return result;
 };
 
-export const useMount = (callback) => {
+export const useMount = (callback: () => void) => {
   useEffect(() => {
     callback();
   }, []);
 };
 
-export const useDebounce = (value, delay) => {
+// 使用到这个自定义hook的地方是useEffect的依赖值，所以要用响应式数据useState
+export const useDebounce = <T>(value: T, delay?: number) => {
   const [debounceValue, setDebouncedValue] = useState(value);
   useEffect(() => {
     const timeout = setTimeout(() => setDebouncedValue(value), delay);
