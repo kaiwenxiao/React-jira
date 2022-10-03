12c48208af4defeff8c6a5006feace1262fd5104
xiao kaiwen <xiaokw@dragonpass.com.cn>
2022-10-03 10:17:38 +0800

    style: 安装emotion，优化登录注册样式

--- src/unauthenticated-app/register.tsx
+++ src/unauthenticated-app/register.tsx
@@ -1,6 +1,7 @@
 import { useAuth } from "context/auth-context";
 import React from "react";
 import { Form, Button, Input } from "antd";
+import { LongButton } from "unauthenticated-app";
 
 export const RegisterScreen = () => {
   const { register, user } = useAuth();
@@ -19,9 +20,9 @@ export const RegisterScreen = () => {
         <Input placeholder={"密码"} type="password" id={"password"} />
       </Form.Item>
       <Form.Item>
-        <Button htmlType={"submit"} type={"primary"}>
+        <LongButton htmlType={"submit"} type={"primary"}>
           注册
-        </Button>
+        </LongButton>
       </Form.Item>
     </Form>
   );
