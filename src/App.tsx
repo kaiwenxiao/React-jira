d3c7895db42bd709b6b9f6b6436397520c661fdb
xiao kaiwen <xiaokw@dragonpass.com.cn>
2022-10-04 23:23:33 +0800

    feat: 添加路由Projects和Project

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import { AuthenticatedApp } from "authenticated-app";
 import { UnauthenticatedApp } from "unauthenticated-app";
 import { ErrorBoundary } from "./components/error-boundary";
 import { FullPageErrorFallback } from "./components/lib";
+import { BrowserRouter as Router } from "react-router-dom";
 
 function App() {
   const { user } = useAuth();
--- src/authenticated-app.tsx
+++ src/authenticated-app.tsx
@@ -7,40 +7,56 @@ import { ReactComponent as SoftwareLogo } from "assets/software-logo.svg";
 import styled from "@emotion/styled";
 import { Row } from "components/lib";
 import { Button, Dropdown, Menu } from "antd";
+import { Navigate, BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { ProjectScreen } from "./screens/project";
 
 export const AuthenticatedApp = () => {
-  const { logout, user } = useAuth();
   return (
     <Container>
-      <Header between={true}>
-        <HeaderLeft gap={true}>
-          {/*<img src={softwareLogo} />*/}
-          <SoftwareLogo width={"18rem"} color={"rgb(38, 132, 255)"} />
-          <h3>项目</h3>
-          <h3>用户</h3>
-        </HeaderLeft>
-        <HeaderRight>
-          <Dropdown
-            overlay={
-              <Menu>
-                <Menu.Item key={"logout"}>
-                  <Button type={"link"} onClick={logout}>
-                    登出
-                  </Button>
-                </Menu.Item>
-              </Menu>
-            }
-          >
-            <Button onClick={(e) => e.preventDefault()}>Hi, {user?.name}</Button>
-          </Dropdown>
-        </HeaderRight>
-      </Header>
+      <PageHeader />
       <Main>
-        <ProjectListScreen />
+        <Router>
+          {/*  ProjectListScreen使用了Link - 应该在Router里面*/}
+          <Routes>
+            <Route path={"/projects"} element={<ProjectListScreen />} />
+            <Route path={"/projects/:projectId/*"} element={<ProjectScreen />} />
+          </Routes>
+        </Router>
       </Main>
     </Container>
   );
 };
+
+const PageHeader = () => {
+  const { logout, user } = useAuth();
+
+  return (
+    <Header between={true}>
+      <HeaderLeft gap={true}>
+        {/*<img src={softwareLogo} />*/}
+        <SoftwareLogo width={"18rem"} color={"rgb(38, 132, 255)"} />
+        <h3>项目</h3>
+        <h3>用户</h3>
+      </HeaderLeft>
+      <HeaderRight>
+        <Dropdown
+          overlay={
+            <Menu>
+              <Menu.Item key={"logout"}>
+                <Button type={"link"} onClick={logout}>
+                  登出
+                </Button>
+              </Menu.Item>
+            </Menu>
+          }
+        >
+          <Button onClick={(e) => e.preventDefault()}>Hi, {user?.name}</Button>
+        </Dropdown>
+      </HeaderRight>
+    </Header>
+  );
+};
+
 const Container = styled.div`
   display: grid;
   grid-template-rows: 6rem 1fr 6rem; // 高度分成三段，每段的高度，1fr自适应
