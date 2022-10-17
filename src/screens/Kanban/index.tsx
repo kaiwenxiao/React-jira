7684ea4e4ecc0de8434f4ceb52ce61a66d71969f
xiao kaiwen <xiaokw@dragonpass.com.cn>
2022-10-17 23:16:01 +0800

    style: 优化看板样式

--- src/screens/Kanban/index.tsx
+++ src/screens/Kanban/index.tsx
@@ -5,6 +5,7 @@ import { useKanbanSearchParams, useProjectInUrl } from "./util";
 import { KanbanColumn } from "./kanban-column";
 import styled from "@emotion/styled";
 import { SearchPanel } from "./search-panel";
+import { ScreenContainer } from "../../components/lib";
 
 export const KanbanScreen = () => {
   useDocumentTitle("看板列表");
@@ -12,7 +13,7 @@ export const KanbanScreen = () => {
   const { data: currentProject } = useProjectInUrl();
   const { data: kanbans } = useKanbans(useKanbanSearchParams());
   return (
-    <div>
+    <ScreenContainer>
       <h1>{currentProject?.name}看板</h1>
       <SearchPanel />
       <ColumnContainer>
@@ -20,12 +21,11 @@ export const KanbanScreen = () => {
           <KanbanColumn kanban={kanban} key={kanban.id} />
         ))}
       </ColumnContainer>
-    </div>
+    </ScreenContainer>
   );
 };
 
 const ColumnContainer = styled.div`
   display: flex;
-  overflow: hidden;
-  margin-right: 2rem;
+  flex: 1;
 `;
