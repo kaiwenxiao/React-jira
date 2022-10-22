7f15b1f3d6e1656cbae01851bbe85331750533a0
xiao kaiwen <xiaokw@dragonpass.com.cn>
2022-10-22 21:17:33 +0800

    feat: 任务组页面开发

--- src/screens/Epic/index.tsx
+++ src/screens/Epic/index.tsx
@@ -1,5 +1,63 @@
-import React from "react";
+import React, { useState } from "react";
+import { Row, ScreenContainer } from "../../components/lib";
+import { useProjectInUrl, useTasksSearchParams } from "../Kanban/util";
+import { useDeleteEpic, useEpics } from "../../utils/epic";
+import { useEpicSearchParams, useEpicsQueryKey } from "./util";
+import { Button, List } from "antd";
+import dayjs from "dayjs";
+import { useTasks } from "../../utils/task";
+import { Link } from "react-router-dom";
+import { CreateEpic } from "./create-epic";
 
 export const EpicScreen = () => {
-  return <h1>Epic</h1>;
+  const { data: currentProject } = useProjectInUrl();
+  const { data: epic } = useEpics(useEpicSearchParams());
+  const { data: tasks } = useTasks({ projectId: currentProject?.id });
+  const { mutate: deleteEpic } = useDeleteEpic(useEpicsQueryKey());
+  const [epicCreateOpen, setEpicCreateOpen] = useState(false);
+
+  return (
+    <ScreenContainer>
+      <Row between={true}>
+        <h1>{currentProject?.name}任务组</h1>
+        <Button onClick={() => setEpicCreateOpen(true)}>创建任务组</Button>
+      </Row>
+      <List
+        style={{ overflowY: "scroll" }}
+        dataSource={epic}
+        itemLayout={"vertical"}
+        renderItem={(epic) => (
+          <List.Item>
+            <List.Item.Meta
+              title={
+                <Row between={true}>
+                  <span>{epic.name}</span>
+                  <Button type={"link"}>删除</Button>
+                </Row>
+              }
+              description={
+                <div>
+                  <div>开始时间：{dayjs(epic.start).format("YYYY-MM-DD")}</div>
+                  <div>结束时间：{dayjs(epic.end).format("YYYY-MM-DD")} </div>
+                </div>
+              }
+            />
+            <div>
+              {tasks
+                ?.filter((task) => task.epicId === epic.id)
+                .map((task) => (
+                  <Link
+                    to={`/projects/${currentProject?.id}/kanban?editingTaskId=${task.id}`}
+                    key={task.id}
+                  >
+                    {task.name}
+                  </Link>
+                ))}
+            </div>
+          </List.Item>
+        )}
+      />
+      <CreateEpic onClose={() => setEpicCreateOpen(false)} visible={epicCreateOpen} />
+    </ScreenContainer>
+  );
 };
